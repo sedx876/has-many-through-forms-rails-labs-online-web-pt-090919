ecdb00c44bb798fef37e6d2f886d04ffa63f2228
Sharon Watkins <swatkins876@gmail.com>
2020-02-20 17:15:10 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,11 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    compar = params.require(:comment)
+    if compar[:user_attributes][:username].length > 0
+      compar.permit(:content, :post_id, user_attributes:[:username])
+    else
+      compar.permit(:content, :post_id, :user_id)
+    end
   end
 end
