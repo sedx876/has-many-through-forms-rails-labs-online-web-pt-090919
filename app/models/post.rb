307407676145ed6cacc9315af4f50775b1fb5a41
Sharon Watkins <swatkins876@gmail.com>
2020-02-20 17:17:56 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
 
+    def all_cats
+      self.categories.map { |cat|
+        cat.name
+      }.join(", ")
+    end
+
+    def all_users
+      self.users.map { |cat|
+        cat.username
+      }.join(", ")
+    end
 end
